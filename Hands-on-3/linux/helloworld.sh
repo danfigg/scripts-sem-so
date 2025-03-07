bc1166b1ede515d390ca83f6f5c8622491992657
Daniel <dangarciafigueroa19@gmail.com>
2025-03-06 21:41:48 -0600

    feat: add helloworls.sh script

---
+++ Hands-on-3/linux/helloworld.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Imprimir en pantalla "Hello World"
+echo "Hello World"
+
+# Listar el contenido del directorio actual
+echo "Contenido del directorio actual:"
+ls
+
+# Crear un directorio llamado Test
+mkdir Test
+
+# Cambiarse al directorio Test
+cd Test
+
+# Listar el contenido del directorio Test (estará vacío al principio)
+echo "Contenido del directorio Test:"
+ls
